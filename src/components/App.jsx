22f08ca8f853508c3c324bd3e151c78e1f9151f8
Gabriel Clemente <clementeaquipresente@yahoo.com>
2022-01-19 12:06:07 +0100

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+import Avatar from "./Avatar";
+
+function createCard(contact) {
+  return (
+    <Card
+      key={contact.id}
+      name={contact.name}
+      img={contact.imgURL}
+      tel={contact.num}
+      email={contact.email}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Avatar img="https://pbs.twimg.com/profile_images/1388575485614084099/DtlQdop__400x400.jpg" />
+      {contacts.map(createCard)}
+    </div>
+  );
+}
+
+export default App;
